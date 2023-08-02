93e399cb261332f89947b75e0ba4861cdc6b548d
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2023-08-01 23:51:56 -0300

    feat: operadores, comentarios e tipos de variaveis

---
+++ operadores.js
@@ -0,0 +1,23 @@
+const typeNumber = 10;
+const numberCalcular = 25;
+
+function soma(a, b) {
+  return a + b;
+}
+
+function subtrair(a, b) {
+  return a - b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+console.log("soma", soma(typeNumber, numberCalcular));
+console.log("subtrair", subtrair(typeNumber, numberCalcular));
+console.log("dividir", dividir(typeNumber, numberCalcular));
+console.log("multiplicar", multiplicar(typeNumber, numberCalcular));
