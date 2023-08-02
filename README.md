89ad9fb1fbd0abd8d2d12ebdcb2bada7ed0232e6
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2023-08-01 23:30:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+1 - Aula
+
+- Tipos de dados
+
+* Boollean
+* Null
+* Undefined
+* Number
+* String
+* Objetos
---
+++ index.js
@@ -0,0 +1,6 @@
+const typeString = "Erickson";
+const typeNumber = 10;
+const typeBoolean = true;
+const typeNull = null;
+const typeUndefined = undefined;
+const typeObject = { name: typeString, number: typeNumber };
